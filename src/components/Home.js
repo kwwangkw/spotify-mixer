ade51a360f9e6a374bdf067bc71e1d2bf472b710
Richard Guan <rguan72@gmail.com>
2021-01-05 14:30:46 -0500

    Remove extraneous code and unused imports now that safeAPI always fetches auth tokens

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,10 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
-import firebaseInst from "../firebase"
-import { setAxiosTokenHeader } from "../utils/auth"
-import { groupsCollection, usersCollection } from "../utils/constants"
-import { createAndFillPlaylist, joinGroup, checkIsInGroup, getUserGroups } from "../utils/data"
+import { getUserGroups } from "../utils/data"
 import { safeAPI, signOut } from "../utils/auth"
-import { useScrollRestoration } from "gatsby"
 
 export default function Home({ user }) {
-    const groupId = "IYMcWpozvPVtGglzUwwN"
-
-    const [token, setToken] = useState("")
-    const [expireTime, setExpireTime] = useState(0)
     const [artists, setArtists] = useState("")
-    const [isInGroup, setIsInGroup] = useState(true)
-    const [playlistLink, setPlaylistLink] = useState("")
     const [groups, setGroups] = useState([])
 
     useEffect(() => {
@@ -24,38 +14,24 @@ export default function Home({ user }) {
         function getFavArtist() {
             axios.get("https://api.spotify.com/v1/me/top/artists").then(res => setArtists(res.data.items[0].name));
         }
-        const db = firebaseInst.firestore()
-        db.collection(usersCollection).doc(user.uid).get().then(doc => {
-            if (doc.exists) {
-                const data = doc.data()
-                setAxiosTokenHeader(data.curr_token)
-                setToken(data.curr_token)
-                setExpireTime(data.expire_time)
-                return data
-            } else {
-                console.log("user does not exist")
-            }
-        }).then((data) => {
-            getUserGroups(user.uid).then(val => {
-                setGroups(val)
-            })
-            safeAPI(user.uid, getFavArtist)
+        getUserGroups(user.uid).then(val => {
+            setGroups(val)
         })
-        console.log(user)
+        safeAPI(user.uid, getFavArtist)
     }, [])
-    console.log(groups)
     return (
         <div>
-            <div>Your tokens are {token}. Artist: {artists}</div>
+            <div>Your fav artist: {artists}</div>
             <div>
                 <p>Groups:</p>
-                {groups.map(group => <a key={group.id} href={`${process.env.BASE_URI}/app/group/${group.id}`}>{group.name}</a>)}
+                {groups.map(group => <div><a key={group.id} href={`${process.env.BASE_URI}/app/group/${group.id}`}>{group.name}</a></div>)}
             </div>
             <button>
                 <a href="/app/newg">
                     New Group
                 </a>
             </button>
+            <br/>
             <button
                 className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
                 onClick={signOut}
