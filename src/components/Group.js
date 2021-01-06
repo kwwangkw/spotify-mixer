79d75d388ee749008f0c7b45998a502bbd44c3ec
kwwangkw <kwwangkw@umich.edu>
2021-01-05 19:07:47 -0500

    basic layout for Group page created playlist

--- src/components/Group.js
+++ src/components/Group.js
@@ -195,7 +195,7 @@ export default function Group({ user, groupId }) {
                                 onClick={() => {
                                     copyToClipboard()
                                     setIsCopied(true)
-                                    setCopyTimeout(2000)
+                                    setCopyTimeout(1000)
                                 }}
                         >
                                 <svg 
@@ -244,50 +244,117 @@ export default function Group({ user, groupId }) {
     }
 
     return (
-        <div className="bg-dark-gray text-primary-400 w-full h-screen font-sans">
-            <div className="w-full h-full flex flex-col justify-center text-center items-center">
-                <h1 className="text-white font-medium text-5xl mb-12">{groupName}</h1>
-                <button
-                    style={{'outline': 'none'}}
-                    className="text-dark-gray font-extralight bg-primary-500 text-xl text-center rounded-full py-1 px-5 flex flex-row mb-3 hover:bg-primary-400 transition duration-300 ease-in-out"
-                    onClick={async () => {
-                        const playlist = await createAndFillPlaylist(user, groupId, "test playlist", "medium_term", 10)
-                        setPlaylistLink(playlist.external_urls.spotify)
-                    }}
-                    disabled={refreshToken === "" || expireTime === 0}
-                >
-                    Generate Playlist
-                </button>
-                <div>
-                    Playlist Link: {playlistLink}
+        <div className="bg-dark-gray text-primary-400 w-full font-sans">
+            <div className="w-full h-full flex flex-col text-center items-center px-4 pt-20 mb-20">
+                <div className="flex flex-row">
+                    <h1 className="text-white font-medium text-5xl mb-3">{groupName}<span></span></h1>
+                    <button style={{'outline': 'none'}} className="relative group text-primary-400 mb-3 text-5xl hover:text-primary-300 transition duration-300 ease-in-out"
+                            onClick={() => {
+                                copyToClipboard()
+                                setIsCopied(true)
+                                setCopyTimeout(1000)
+                            }}
+                    >
+                            <svg 
+                                className="ml-3 h-5 w-5 text-white-500"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />  
+                                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+                            </svg>
+                        <span className={`${ !isCopied ? `block` : `hidden` } absolute z-1 text-xs text-left font-thin invisible ml-1 group-hover:visible `} style={{'top': '15px', 'left' : '110%', 'width': '120px'}}>Copy Invite Link</span>
+                        <span className={`${ isCopied ? `block` : `hidden` } absolute z-1 text-xs text-left font-thin ml-1 `} style={{'top': '15px', 'left' : '110%', 'width': '120px'}}>Copied!</span>
+                    </button>
                 </div>
-                <div>
-                    Share link: {window.location.href}
-                </div>
-                <div>
-                    <p>Group Members:</p>
-                    {groupMembers.map(member => <p key={member}>{member}</p>)}
-                </div>
-                <div className="list-of-tracks">
-                    {playlistTracks && playlistTracks.map((track) => (
-                        <div>
-                            <p>{track.name}</p>
-                            {track.artists && track.artists.map((artist) => (
-                                <p>{artist}</p>
-                            ))}
-                            <img src={track.image_url} alt="soemone's track" />
-                        </div>
-                    ))}
+                <div className="text-gray-400 text-lg mb-3">
+                    <span> &#183; </span>{groupMembers.map(member => <span key={member}>{member} &#183; </span>)}
                 </div>
                 <button
                     style={{'outline': 'none'}}
-                    className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+                    className="py-1 text-white font bg-gray-500 text-center rounded-full px-5 flex flex-row mb-3 hover:bg-gray-400 transition duration-300 ease-in-out"
                 >
                     <a href={"/app/home"}>
-                        Cancel
+                        Home
                     </a>
                 </button>
             </div>
+            <div className="w-full h-full flex flex-col lg:flex-row pb-20 lg:pl-32">
+                <div id="left" className="lg:w-1/4 text-center mb-20 lg:mb-0 lg:mr-20">
+                    <h2 className="mb-10 text-white font-light">{playlistTracks.length} Tracks</h2>
+                    <button
+                        style={{'outline': 'none'}}
+                        className="text-white bg-primary-500 font-semibold text-center rounded-full py-1 px-5 mb-3 hover:bg-primary-400 transition duration-300 ease-in-out"
+                        onClick={async () => {
+                            const playlist = await createAndFillPlaylist(user, groupId, "test playlist", "medium_term", 10)
+                            setPlaylistLink(playlist.external_urls.spotify)
+                            console.log(playlist)
+                        }}
+                        disabled={refreshToken === "" || expireTime === 0}
+                    >
+                        Update Playlist
+                    </button>
+                </div>
+                <div id="right" className="w-full ml-5 md:ml-20 lg:ml-0 pr-20">
+                    <div className="list-of-tracks w-full -mt-3">
+                        {playlistTracks && playlistTracks.map((track) => (
+                            <div className="flex flex-row w-full px-4 py-1 mb-4 hover:bg-gray-900 transition duration-300 ease-in-out">
+                                <img className="mr-3" width="75px" height="75px" src={track.image_url} alt={track.name} />
+                                <div id="desc" className=" flex flex-col justify-center">
+                                    <div>
+                                        <p className="text-white mb-1">{track.name}</p>
+                                        {track.artists && track.artists.map((artist) => (
+                                            <span className="text-gray-400">{artist} &#183; </span>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            
+            {/*
+            <div className="bg-dark-gray text-primary-400 w-full h-screen font-sans">
+                <div className="w-full h-full flex flex-col justify-center text-center items-center">
+                    <h1 className="text-white font-medium text-5xl mb-12">{groupName}</h1>
+                    <button
+                        style={{'outline': 'none'}}
+                        className="text-dark-gray font-extralight bg-primary-500 text-xl text-center rounded-full py-1 px-5 flex flex-row mb-3 hover:bg-primary-400 transition duration-300 ease-in-out"
+                        onClick={async () => {
+                            const playlist = await createAndFillPlaylist(user, groupId, "test playlist", "medium_term", 10)
+                            setPlaylistLink(playlist.external_urls.spotify)
+                        }}
+                        disabled={refreshToken === "" || expireTime === 0}
+                    >
+                        Update Playlist
+                    </button>
+                    <div>
+                        Playlist Link: {playlistLink}
+                    </div>
+                    <div>
+                        Share link: {window.location.href}
+                    </div>
+                    <div>
+                        <p>Group Members:</p>
+                        {groupMembers.map(member => <p key={member}>{member}</p>)}
+                    </div>
+                    
+                    <button
+                        style={{'outline': 'none'}}
+                        className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+                    >
+                        <a href={"/app/home"}>
+                            Cancel
+                        </a>
+                    </button>
+                </div>
+            </div>
+            */}
         </div>
     )
 }
