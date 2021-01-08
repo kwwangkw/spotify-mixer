f393473dc9107a3a2c672b622207e2aadab9fb65
kwwangkw <kwwangkw@umich.edu>
2021-01-08 18:07:13 -0500

    home page formatting including truncating overflow text

--- src/components/Home.js
+++ src/components/Home.js
@@ -59,21 +59,21 @@ export default function Home({ user }) {
                 <h1 className="text-white text-4xl mb-3 text-center md:text-left">Personal Insights</h1>
                 <ul className="grid gap-4 grid-cols-1 md:gap-8">
                     <ul className="grid gap-4 grid-cols-1 lg:grid-cols-2 md:gap-8">
-                        <a href={artists[0].external_urls['spotify']} target="_blank" className="-mb-5 sm:-mb-10 lg:mb-0">
+                        <a href={artists[0] && artists[0].external_urls['spotify']} target="_blank" className="-mb-3 md:-mb-10 lg:mb-0">
                             <li className="p-5 md:p-8 bg-gradient-to-br from-pink-200 to-pink-400 h-40 transform hover:scale-105 transition duration-400 ease-in-out rounded-2xl flex items-center text-3xl text-white">
                                 <img className="mr-4 rounded-full" width="130px" height="130px" src={artists[0].images[0]['url']} alt={artists[0].name}></img>
-                                <div>
-                                    <h3 >Your Top Artist</h3>
+                                <div className="truncate w-4/5">
+                                    <h3 className="">Your Top Artist</h3>
                                     <h3 className="md:text-4xl text-dark-gray my-1">{artists[0].name}</h3>
                                     <h4 className="text-2xl text-white">Ranking: {artists[0].popularity}</h4>
                                 </div>
                             </li>
                         </a>
-                        <a href={artists[1].external_urls['spotify']} target="_blank" className="invisible lg:visible -mb-48 lg:mb-0">
+                        <a href={artists[1] && artists[1].external_urls['spotify']} target="_blank" className="invisible lg:visible -mb-40 lg:mb-0">
                             <li className="p-5 md:p-8 bg-gradient-to-br from-indigo-200 to-indigo-400 h-40 transform hover:scale-105 transition duration-400 ease-in-out rounded-2xl flex items-center text-3xl text-white">
                                 <img className="mr-4 rounded-full" width="130px" height="130px" src={artists[1].images[0]['url']} alt={artists[0].name}></img>
-                                <div>
-                                    <h3 >Your Runner-up Artist</h3>
+                                <div className="truncate w-4/5">
+                                    <h3 className="">Your Runner-up Artist</h3>
                                     <h3 className="text-4xl text-dark-gray my-1">{artists[1].name}</h3>
                                     <h4 className="text-2xl text-white">Ranking: {artists[1].popularity}</h4>
                                 </div>
@@ -81,42 +81,56 @@ export default function Home({ user }) {
                         </a>
                     </ul>
                     <ul className="grid gap-4 grid-cols-1 lg:grid-cols-3 md:gap-8">
-                        <li className="visible -mb-44 lg:mb-0 p-3 bg-gradient-to-br from-cyan-200 to-cyan-400 h-40 rounded-2xl flex items-center text-3xl text-white">
-                            {tracks[0] && <img className="mr-4 rounded" width="130px" height="130px" src={tracks[0].album.images[1]['url']} alt={tracks[0].name}></img>}
-                            {tracks[0] && (
-                                <div>
-                                    <h3 >#1 Song</h3>
-                                    <h3 className="text-4xl text-dark-gray my-1">{tracks[0].name}</h3>
-                                    {tracks[0] && tracks[0].artists.map((artist, index) => (
-                                        <h3 key={index} className="text-2xl text-white">{artist.name} {(index + 1 === tracks[0].artists.length) ? "" : (<span>&#183;</span>)} </h3>
-                                    ))}
-                                </div>
-                            )}
-                        </li>
-                        <li className="invisible lg:visible p-3 bg-gradient-to-br from-primary-200 to-primary-400 h-40 rounded-2xl flex items-center text-3xl text-white">
-                            {tracks[1] && <img className="mr-4 rounded" width="130px" height="130px" src={tracks[1].album.images[1]['url']} alt={tracks[1].name}></img>}
-                            {tracks[1] && (
-                                <div>
-                                    <h3 >#2 Song</h3>
-                                    <h3 className="text-4xl text-dark-gray my-1">{tracks[1].name}</h3>
-                                    {tracks[1] && tracks[1].artists.map((artist, index) => (
-                                        <h3 key={index} className="text-2xl text-white">{artist.name} {(index + 1 === tracks[0].artists.length) ? "" : (<span>&#183;</span>)} </h3>
-                                    ))}
-                                </div>
-                            )}
-                        </li>
-                        <li className="invisible lg:visible p-3 bg-gradient-to-br from-yellow-200 to-yellow-400 h-40 rounded-2xl flex items-center text-3xl text-white">
+                        <a href={tracks[0] && tracks[0].external_urls['spotify']} target="_blank">
+                            <li className="visible -mb-44 lg:mb-0 p-3 bg-gradient-to-br from-cyan-200 to-cyan-400 h-40 rounded-2xl flex items-center text-3xl text-white transform hover:scale-105 transition duration-400 ease-in-out">
+                                {tracks[0] && <img className="mr-4 rounded" width="130px" height="130px" src={tracks[0].album.images[1]['url']} alt={tracks[0].name}></img>}
+                                {tracks[0] && (
+                                    <div className="truncate md:w-4/5">
+                                        <h3 className="">#1 Song</h3>
+                                        <h3 className="text-4xl text-dark-gray my-1">{tracks[0].name}</h3>
+                                        <div className="flex flex-row">
+                                            {tracks[0] && tracks[0].artists.map((artist, index) => (
+                                                <h3 key={index} className="text-2xl text-white">{artist.name}{(index + 1 === tracks[0].artists.length) ? "" : (<span>&#183;</span>)} </h3>
+                                            ))}
+                                        </div> 
+                                    </div>
+                                )}
+                            </li>
+                        </a>
+                        <a href={tracks[1] && tracks[1].external_urls['spotify']} target="_blank">
+                            <li className="invisible lg:visible p-3 bg-gradient-to-br from-primary-200 to-primary-400 h-40 rounded-2xl flex items-center text-3xl text-white transform hover:scale-105 transition duration-400 ease-in-out">
+                                {tracks[1] && <img className="mr-4 rounded" width="130px" height="130px" src={tracks[1].album.images[1]['url']} alt={tracks[1].name}></img>}
+                                {tracks[1] && (
+                                    <div className="truncate w-4/5">
+                                        <h3 className="">#2 Song</h3>
+                                        <h3 className="text-4xl text-dark-gray my-1">{tracks[1].name}</h3>
+                                        <div className="flex flex-row">
+                                            {tracks[1] && tracks[1].artists.map((artist, index) => (
+                                                <h3 key={index} className="text-2xl text-white">{artist.name}{(index + 1 === tracks[1].artists.length) ? "" : (<span>&#183;</span>)} </h3>
+                                            ))}
+                                        </div> 
+                                    </div>
+                                )}
+                            </li>
+                        </a>
+                        <a href={tracks[2] && tracks[2].external_urls['spotify']} target="_blank">
+                        <li className="invisible lg:visible p-3 bg-gradient-to-br from-yellow-200 to-yellow-400 h-40 rounded-2xl flex items-center text-3xl text-white transform hover:scale-105 transition duration-400 ease-in-out">
                             {tracks[2] && <img className="mr-4 rounded" width="130px" height="130px" src={tracks[2].album.images[1]['url']} alt={tracks[2].name}></img>}
                             {tracks[2] && (
-                                <div>
-                                    <h3 >#3 Song</h3>
+                                <div className="truncate w-4/5">
+                                    <h3 className="">#3 Song</h3>
                                     <h3 className="text-4xl text-dark-gray my-1">{tracks[2].name}</h3>
-                                    {tracks[2] && tracks[2].artists.map((artist, index) => (
-                                        <h3 key={index} className="text-2xl text-white">{artist.name} {(index + 1 === tracks[0].artists.length) ? "" : (<span>&#183;</span>)} </h3>
-                                    ))}
+                                    <div className="w-56 md:w-96 lg:w-72">
+                                        <p className="flex flex-row">
+                                            {tracks[2] && tracks[2].artists.map((artist, index) => (
+                                                <h3 key={index} className="text-2xl text-white">{artist.name}{(index + 1 === tracks[2].artists.length) ? "" : (<span>&#183;</span>)} </h3>
+                                            ))}
+                                        </p>
+                                    </div> 
                                 </div>
                             )}
                         </li>
+                        </a>
                     </ul>
                 </ul>
             </div>
@@ -127,7 +141,7 @@ export default function Home({ user }) {
                     {groups.map(group => 
                         <div key={group.id}>
                             <Link to={`/app/group/${group.id}`}>
-                                <li className="overflow-hidden p-4 bg-gradient-to-br from-gray-600 to-gray-700 h-40 text-gray-300 transform hover:scale-105 transition duration-400 ease-in-out rounded-xl flex justify-center items-center text-center text-4xl font-light">
+                                <li className="break-all overflow-hidden p-4 bg-gradient-to-br from-gray-600 to-gray-700 h-40 text-gray-300 transform hover:scale-105 transition duration-400 ease-in-out rounded-xl flex justify-center items-center text-center text-4xl font-light">
                                     <div className="flex flex-col">
                                         <h2>{group.name}</h2>
                                     </div>
