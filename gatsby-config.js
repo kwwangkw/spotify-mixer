b342a619851a6c4d9c70cb0390cc86efab94fb50
Richard Guan <rguan72@gmail.com>
2021-01-15 10:28:24 -0500

    Add gatsby-plugin-netlify to auto add redirect routes

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-source-filesystem",
       options: {
